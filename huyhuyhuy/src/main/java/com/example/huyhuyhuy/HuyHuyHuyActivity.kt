5c41446a395acc2df10871e15f20b356c9248bd0
Truong Tran Quang Huy <truongtranquanghuy@gmail.com>
2019-07-11 15:29:26 +0700

    add huyhuyhuy module

---
+++ huyhuyhuy/src/main/java/com/example/huyhuyhuy/HuyHuyHuyActivity.kt
@@ -0,0 +1,12 @@
+package com.example.huyhuyhuy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HuyHuyHuyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_huy_huy_huy)
+    }
+}
